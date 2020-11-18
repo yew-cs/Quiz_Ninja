6c51748d3e20d1747fa41580148ccffb0597dc08
Chuo Shing Yew <yewchuoshing@gmail.com>
2020-11-18 16:12:31 +0800

    Working with Document Object Model

--- main.js
+++ main.js
@@ -3,12 +3,26 @@ const quiz = [
     { name: "Wonder Woman", realName: "Diana Prince" },
     { name: "Batman", realName: "Bruce Wayne" },
 ];
+const view = {
+    score: document.querySelector("#score strong"),
+    question: document.getElementById("question"),
+    result: document.getElementById("result"),
+    info: document.getElementById("info"),
+    render(target, content, attributes) {
+        for (const key in attributes) {
+            target.setAttribute(key, attributes[key]);
+        }
+        target.innerHTML = content;
+    }
+}
 
 const game = {
     start(quiz) {
+        alert('Welcome to Quiz Ninja!');
         this.questions = [...quiz];
         this.score = 0;
         //main game loop
+
         for (const question of this.questions) {
             this.question = question;
             this.ask();
@@ -16,22 +30,27 @@ const game = {
         //end of main game loop
         this.gameOver();
     },
+
     ask() {
         const question = `What is ${this.question.name}'s real name?`;
         const response = prompt(question);
+        view.render(view.question, question);
         this.check(response);
     },
     check(response) {
         const answer = this.question.realName;
         if (response === answer) {
+            view.render(view.result, 'Correct!', { 'class': 'correct' });
             alert('Correct!');
             this.score++;
+            view.render(view.score, this.score);
         } else {
+            view.render(view.result, `Wrong!The correct answer was ${answer}.`, { 'class': 'wrong' });
             alert(`Wrong!The correct answer was ${answer}.`);
         }
     },
     gameOver() {
-        alert(`Game over! Your score is ${this.score} point${this.score !== 1 ? 's' : ''}`);
+        view.render(view.info, `Game over! Your score is ${this.score} point${this.score !== 1 ? 's' : ''}`);
     }
 }
 game.start(quiz);
