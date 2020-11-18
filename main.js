8d326e0c5a9e06d15ad3bb176b1bce535af6a902
Chuo Shing Yew <yewchuoshing@gmail.com>
2020-11-18 11:05:16 +0800

    Refactoring the Quiz Ninja Project using function

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
 //Sets the questions in an array!
 const quiz = [
-	["What is Superman's real name?", "Clark Kent"],
-	["What is Wonder Woman's real name?", "Diana Prince"],
-	["What is Batman's real name?", "Bruce Wayne"]
-	];
+    ["What is Superman's real name?", "Clark Kent"],
+    ["What is Wonder Woman's real name?", "Diana Prince"],
+    ["What is Batman's real name?", "Bruce Wayne"]
+];
 
 //initialize score
-let score = 0;
+function start(quiz) {
+    let score = 0;
 
-//Get response to questions from player
-for (const[questions,answer]of quiz){
-	const response = prompt(questions);
-	if(response ===answer){
-		alert('Correct!');
-		score++;
-	}else{
-		alert(`Wrong!The correct answer was ${answer}`);
-	}
+    //main game loop
+    for (const [question, answer] of quiz) {
+        const response = ask(question);
+        check(response, answer);
+    }
+    //end of main game loop
+
+    gameOver();
+
+    // function declarations
+    function ask(question) {
+        return prompt(question);
+    }
+
+    function check(response, answer) {
+        if (response === answer) {
+            alert('Correct!');
+            score++;
+        } else {
+            alert(`Wrong!The correct answer was ${answer}.`);
+        }
+    }
+
+    //End of game, report the player's score
+    function gameOver() {
+        alert(`Game over! Your score is ${score} point${score !== 1 ? 's' : ''}`);
+    }
 }
-
-//End of game, report the player's score
-alert(`Game over! Your score is ${score} point${score !== 1 ? 's' :''}`);
+start(quiz);
