ed074ca05cce8b9e916af28efe53d345dcac1d0a
Chuo Shing Yew <yewchuoshing@gmail.com>
2020-11-18 17:13:18 +0800

    Working with Forms

--- main.js
+++ main.js
@@ -3,65 +3,92 @@ const quiz = [
     { name: "Wonder Woman", realName: "Diana Prince" },
     { name: "Batman", realName: "Bruce Wayne" },
 ];
-const view = {
-    score: document.querySelector("#score strong"),
-    question: document.getElementById("question"),
-    result: document.getElementById("result"),
-    info: document.getElementById("info"),
 
+const view = {
+    score: document.querySelector('#score strong'),
+    question: document.getElementById('question'),
+    result: document.getElementById('result'),
+    info: document.getElementById('info'),
+    response: document.querySelector('#response'),
     render(target, content, attributes) {
         for (const key in attributes) {
             target.setAttribute(key, attributes[key]);
         }
         target.innerHTML = content;
     },
-    start: document.getElementById("start"),
+    start: document.getElementById('start'),
+
     show(element) {
-        element.style.display = "block";
+        element.style.display = 'block';
     },
     hide(element) {
-        element.style.display = "none";
+        element.style.display = 'none';
     },
-}
+
+    setup() {
+        this.show(this.question);
+        this.show(this.response);
+        this.show(this.result);
+        this.hide(this.start);
+        this.render(this.score, game.score);
+        this.render(this.result, '');
+        this.render(this.info, '');
+        this.resetForm();
+    },
+
+    resetForm() {
+        this.response.answer.value = '';
+        this.response.answer.focus();
+    },
+
+    teardown() {
+        this.hide(this.response);
+        this.hide(this.question);
+        this.show(this.start);
+    }
+};
 
 const game = {
         start(quiz) {
-            view.hide(view.start);
-            alert('Welcome to Quiz Ninja!');
-            this.questions = [...quiz];
-            this.score = 0;
-            //main game loop
+            this.score = 0; // score iniatilizer
+            this.questions = quiz;
+            view.setup();
+            this.ask();
+        },
 
-            for (const question of this.questions) {
-                this.question = question;
-                this.ask();
+        ask(name) {
+            if (this.questions.length > 0) {
+                this.question = this.questions.pop();
+                const question = `What is ${this.question.name}'s real name?`;
+                view.render(view.question, question);
+            } else {
+                this.gameOver();
             }
-            //end of main game loop
-            this.gameOver();
         },
 
-        ask() {
-            const question = `What is ${this.question.name}'s real name?`;
-            const response = prompt(question);
-            view.render(view.question, question);
-            this.check(response);
-        },
-        check(response) {
+        check(event) {
+            event.preventDefault();
+            const response = view.response.answer.value;
             const answer = this.question.realName;
+
             if (response === answer) {
                 view.render(view.result, 'Correct!', { 'class': 'correct' });
-                alert('Correct!');
                 this.score++;
                 view.render(view.score, this.score);
             } else {
                 view.render(view.result, `Wrong!The correct answer was ${answer}.`, { 'class': 'wrong' });
-                alert(`Wrong!The correct answer was ${answer}.`);
             }
+            view.resetForm();
+            this.ask();
         },
         gameOver() {
             view.render(view.info, `Game over! Your score is ${this.score} point${this.score !== 1 ? 's' : ''}`);
-            view.show(view.start);
+            view.teardown();
         }
     }
     // game.start(quiz);
 view.start.addEventListener('click', () => game.start(quiz), false);
+
+view.response.addEventListener('submit', (event) => game.check(event), false);
+
+view.hide(view.response);
