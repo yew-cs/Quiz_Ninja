877a64590f761ad69c4d851b20cbbacc9cdc4b1f
Chuo Shing Yew <yewchuoshing@gmail.com>
2020-11-19 11:47:48 +0800

    dded random & shuffle question function Quiz Ninja

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+function random(a, b = 1) {
+    //if only 1 argument is provided, we need to swap the value of a and b
+    if (b === 1) {
+        [a, b] = [b, a];
+    }
+    return Math.floor((b - a + 1) * Math.random()) + a;
+};
+
+function shuffle(array) {
+    for (let i = array.length; i; i--) {
+        let j = random(i) - 1;
+        [array[i - 1], array[j]] = [array[j], array[i - 1]];
+    }
+};
+
 const quiz = [
     { name: "Superman", realName: "Clark Kent" },
     { name: "Wonder Woman", realName: "Diana Prince" },
@@ -63,6 +78,7 @@ const game = {
         ask(name) {
             console.log('ask() invoked');
             if (this.questions.length > 0) {
+                shuffle(this.questions);
                 this.question = this.questions.pop();
                 const question = `What is ${this.question.name}'s real name?`;
                 view.render(view.question, question);
